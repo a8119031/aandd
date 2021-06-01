f706d73649cd59fcb1012918db40cd2281d5d2ce
Sae Okano <a8119031@gsuite.si.aoyama.ac.jp>
2021-06-02 00:15:37 +0900

    prac4-4 バイナリサーチ

---
+++ aandd.js
@@ -0,0 +1,43 @@
+function show(obj) {
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
+        }
+        html = html + '</tr>';
+        html = html + '</table>';
+        //html = html + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
+    }
+}
+
+function createSequentialNumbers(array, n) {
+    for (i = 0; i < n; i++) {
+        array.push(i + 1);
+    }
+}
+
+function createRandomNumbers(array, n) {
+    for (i = 0; i < n; i++) {
+        array.push(Math.floor(Math.random() * 10) + 1);
+    }
+}
+
+function createOrderedRandomNumbers(numbers, n) {
+    var count = 0;
+    for (var i = 1; ; i++) {
+        if (Math.floor(Math.random() * 3) == 0) {
+            numbers.push(i);
+            count++;
+            if (count >= n) {
+                break;
+            }
+        }
+    }
+}
